84e30c2e1312459326be2e8ec2c219ffa63ba198
SashankR117 <balijagudamshashank25@gmail.com>
2024-12-19 13:07:10 +0530

    complete files

--- app/src/main/java/com/singlepointsol/androidpixabayassignment/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.singlepointsol.androidpixabayassignment
-
-import retrofit2.Response
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("https://pixabay.com/api/?key={ KEY }&q=cars&image_type=photo")
-    suspend fun getCars(): Response<CarResponse>
-}
